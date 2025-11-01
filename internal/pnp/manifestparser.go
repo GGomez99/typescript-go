d836bb4a15e6c8957c358cf0d70bc50cd54c2f83
gun.yu <ms103boan@gmail.com>
2025-11-02 00:09:43 +0900

    add pnpapi test

--- internal/pnp/manifestparser.go
+++ internal/pnp/manifestparser.go
@@ -81,33 +81,40 @@ func parseManifestFromPath(fs PnpApiFS, manifestDir string) (*PnpManifestData, e
 	if ok {
 		pnpDataString = data
 	} else {
-		pnpScriptString, ok := fs.ReadFile(tspath.CombinePaths(manifestDir, ".pnp.cjs"))
-		if !ok {
-			return nil, errors.New("failed to read .pnp.cjs file")
+		dataString, err := extractPnpDataStringFromCjsPath(fs, tspath.CombinePaths(manifestDir, ".pnp.cjs"))
+		if err != nil {
+			return nil, err
 		}
-
-		manifestRegex := regexp2.MustCompile(`(const[ \r\n]+RAW_RUNTIME_STATE[ \r\n]*=[ \r\n]*|hydrateRuntimeState\(JSON\.parse\()'`, regexp2.None)
-		matches, err := manifestRegex.FindStringMatch(pnpScriptString)
-		if err != nil || matches == nil {
-			return nil, errors.New("We failed to locate the PnP data payload inside its manifest file. Did you manually edit the file?")
-		}
-
-		start := matches.Index + matches.Length
-		var b strings.Builder
-		b.Grow(len(pnpScriptString))
-		for i := start; i < len(pnpScriptString); i++ {
-			if pnpScriptString[i] == '\'' {
-				break
-			}
-
-			if pnpScriptString[i] != '\\' {
-				b.WriteByte(pnpScriptString[i])
-			}
-		}
-		pnpDataString = b.String()
+		pnpDataString = dataString
 	}
 
 	return parseManifestFromData(pnpDataString, manifestDir)
+}
+
+func extractPnpDataStringFromCjsPath(fs PnpApiFS, path string) (string, error) {
+	pnpScriptString, ok := fs.ReadFile(path)
+	if !ok {
+		return "", errors.New("failed to read .pnp.cjs file")
+	}
+	manifestRegex := regexp2.MustCompile(`(const[ \r\n]+RAW_RUNTIME_STATE[ \r\n]*=[ \r\n]*|hydrateRuntimeState\(JSON\.parse\()'`, regexp2.None)
+	matches, err := manifestRegex.FindStringMatch(pnpScriptString)
+	if err != nil || matches == nil {
+		return "", errors.New("we failed to locate the PnP data payload inside its manifest file. Did you manually edit the file?")
+	}
+
+	start := matches.Index + matches.Length
+	var b strings.Builder
+	b.Grow(len(pnpScriptString))
+	for i := start; i < len(pnpScriptString); i++ {
+		if pnpScriptString[i] == '\'' {
+			break
+		}
+
+		if pnpScriptString[i] != '\\' {
+			b.WriteByte(pnpScriptString[i])
+		}
+	}
+	return b.String(), nil
 }
 
 func parseManifestFromData(pnpDataString string, manifestDir string) (*PnpManifestData, error) {
